66088f1d30ef4d7def89d8af88e367786069d094
Thomas Deutsch <thomas@tuxpeople.org>
2023-01-06 17:42:37 +0100

    add upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+_step_counter=0
+function step() {
+        _step_counter=$(( _step_counter + 1 ))
+        printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
+}
+
+step "Getting all the infos needed..."
+echo "  - Get latest Talos version"
+TALOS_VERSION=$(curl --silent -qI https://github.com/siderolabs/talos/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}')
+
+echo "  - Get latest Kubernetes version"
+KUBERNETES_VERSION=$(curl --silent -qI https://github.com/siderolabs/kubelet/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}' | sed 's/v//g')
+
+echo "  - Get cluster members count"
+MEMBERS=$(talosctl get members | grep Member | wc -l)
+
+step "Backup ETCD db"
+talosctl etcd snapshot talos-etcd-$(date +%Y%m%d-%H%M%S).snapshot
+
+step "Clean up old ETCD db backups"
+ls -t talos-etcd-* | tail -n +4 | xargs rm --
+
+step "Upgrade Talos OS for all nodes"
+for NODE in $(grep -A${MEMBERS} endpoints ~/.talos/config | tail -${MEMBERS} | awk '{ print $2 }')
+do
+    talosctl upgrade -n ${NODE} --image ghcr.io/siderolabs/installer:$TALOS_VERSION
+done
+
+step "Upgrade talosctl"
+sudo curl --silent -qLo /usr/local/bin/talosctl https://github.com/siderolabs/talos/releases/download/$TALOS_VERSION/talosctl-$(uname -s | tr "[:upper:]" "[:lower:]")-arm64
+
+step "Upgrade Kubernetes version"
+talosctl upgrade-k8s --to ${KUBERNETES_VERSION}
