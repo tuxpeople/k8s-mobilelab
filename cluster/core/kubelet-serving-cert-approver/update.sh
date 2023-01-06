5fec38e8335c7da29a97d85f6eb853a4f1728d17
Thomas Deutsch <thomas@tuxpeople.org>
2023-01-06 21:54:33 +0100

    test

--- cluster/core/kubelet-serving-cert-approver/update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(git rev-parse --show-toplevel)
-FILE="${BASEDIR}/cluster/core/kubelet-serving-cert-approver/patched-standalone-install.yaml"
-
-echo "# yamllint disable rule:indentation" > ${FILE}
-curl -sL https://raw.githubusercontent.com/alex1989hu/kubelet-serving-cert-approver/main/deploy/standalone-install.yaml | grep -v pod-security.kubernetes.io >> ${FILE}
